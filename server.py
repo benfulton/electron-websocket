99a83aca56fd974ae98067af4f3344c548730141
Ben Fulton <ben@benfulton.net>
2021-06-17 20:43:26 -0400

    Initial commit

---
+++ server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+print("Running")
+async def echo(websocket, path):
+    async for message in websocket:
+        await websocket.send(message)
+
+asyncio.get_event_loop().run_until_complete(
+    websockets.serve(echo, 'localhost', 8765))
+asyncio.get_event_loop().run_forever()
+print("Complete")
